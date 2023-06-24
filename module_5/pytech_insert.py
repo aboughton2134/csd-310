4cf7dcbcb1d85d1fb9c02f537ac25dd0cbe2ffda
aboughton2134 <aboughton@my365.bellevue.edu>
2023-06-24 12:20:33 -0500

    Part 2 continued

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -3,21 +3,24 @@
 MongoDB: insert_one()
 ida = {
  “first_name”: “Ida”
+ "last_name": "Hope"
 }
 
 MongoDB: insert_one()
 sara = {
  “first_name”: “Sara”
+ "last_name": "Tachin"
 }
 
 MongoDB: insert_one() 
 linus = {
  “first_name”: “Linus”
+ "last_name": "Yodlee"
 }
 
-ida_student_id = students.insert_one(ida).inserted_id
-sara_student_id = students.insert_one(sara).inserted_id
-linus_student_id = students.insert_one(linus).inserted_id
+ida_student_id = students.insert_one(1007).inserted_id
+sara_student_id = students.insert_one(1008).inserted_id
+linus_student_id = students.insert_one(1009).inserted_id
 
 print(ida_student_id)
 print(sara_student_id)
